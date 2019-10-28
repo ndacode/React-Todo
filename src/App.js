069b3150b1fa0e33e4ec9b58e32ffd830b42559d
Nicole Adams <nicoleadams.boston@gmail.com>
2019-10-28 17:10:45 -0400

    Added class component and a small data set in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+const todos = [
+  {
+  entry: 'Update Resume',
+  id: 0,
+  complete: false
+  },
+  {
+    entry: 'Update Meetup Groups',
+    id: 1,
+    complete: false
+  },
+  {
+    entry: 'Clean out the Garage',
+    id: 2,
+    complete: false
+  }
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor(){
+    super();
+    this.state = {
+      ToDoList: todos
+    };
+  }
+
+  addTodo = entry => {
+    const newTodo = {
+      entry: entry,
+      id: '',
+      complete: false
+    };
+    this.setState({
+      ToDoList: [...this.state.ToDoList, newTodo]
+    });
+  };
+  
+  // toggleTodo = id => {
+  //   this.setState({
+  //     ToDoList: this.state.ToDoList.map(item => {
+  //       if (item.id === id) {
+  //         return {
+  //           ...item,
+  //           complete: !item.complete
+  //         };
+  //       } else {
+  //         return item;
+  //       }
+  //     })
+  //   })
+  // }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
+    console.log('rendering...')
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className= "App">
+        <div className = "header">
+            <h2>Welcome to your Todo App!</h2>
+        </div>
       </div>
     );
   }
